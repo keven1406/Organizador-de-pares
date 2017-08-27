4fb3deed3eae374b4684132940e33b3608f38f65
Elias Keven <eliaskeven2@gmail.com>
2017-08-27 19:20:24 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,7 +3,7 @@ const listaDoCarrinho = []
 //lista:: String -> string -> string -> Array
 const lista = inserir => nome => sexo => estadoCivil => id => 
 	listaDoCarrinho.push({'nome': nome, 'sexo': sexo, 'estadoCivil': estadoCivil, 'ID': id})
-inserirPessoa = lista(listaDoCarrinho)
+const inserirPessoa = lista(listaDoCarrinho)
 inserirPessoa('Jonas Melo')('Masculino')('Solteiro')(listaDoCarrinho.length)
 inserirPessoa('Carlos Mirando')('Masculino')('Casado')(listaDoCarrinho.length)
 inserirPessoa('Elisabeti Sheila')('Feminino')('Solteiro')(listaDoCarrinho.length)
